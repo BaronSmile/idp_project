32df598eb6bccfd1d9f44fed546840d5fb890552
Nurlan <nergashev@fil-it.ru>
2023-08-08 20:22:18 +0700

    delete file

--- server/config.js
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql2";
-
-export const db = mysql.createPool({
-  host: "lordnl69.beget.tech",
-  user: "lordnl69_dota2",
-  password: "111Dota2",
-  database: "lordnl69_dota2"
-});
